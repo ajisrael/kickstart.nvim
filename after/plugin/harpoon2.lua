2e82e5bd697c55d1c93256cfddb18a735ff718ee
Alex Israels <Alex.Israels@Amway.com>
2024-08-20 12:11:40 -0400

    f   - switched harpoon list back to C-e

--- after/plugin/harpoon2.lua
+++ after/plugin/harpoon2.lua
@@ -8,7 +8,7 @@ harpoon:setup()
 vim.keymap.set('n', '<leader>a', function()
   harpoon:list():add()
 end)
-vim.keymap.set('n', '<C-i>', function()
+vim.keymap.set('n', '<C-e>', function()
   harpoon.ui:toggle_quick_menu(harpoon:list())
 end)
 
