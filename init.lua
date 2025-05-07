32b130c811935e8ca3371ad68266a54f5cdab615
Alex Israels <Alex.Israels@Amway.com>
2025-05-07 08:25:02 -0400

    f   - added spell check for markdown files

--- init.lua
+++ init.lua
@@ -138,6 +138,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Turn on spell check for markdown files
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'markdown' },
+  callback = function()
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = 'en_us'
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
